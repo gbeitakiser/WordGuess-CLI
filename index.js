dc9b14da9a56bacd39570ba69af653f36c75d932
Gabriel Beita-Kiser <gbeitakiser@gmail.com>
2018-12-06 15:49:50 -0800

    letters.js working. Now need to work on word.js then index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// Probably needs to require word.js
+
+// * Randomly selects a word and uses the `Word` constructor to store it
+// * Prompts the user for each guess and keeps track of the user's remaining guesses
--- letter.js
+++ letter.js
@@ -1,1 +1,42 @@
-// Should not require any files
+var letterExport = function() {
+    
+//=============================
+//Variables
+// Try to use a function/variable that restricts user input to just letters and no symboms or nubmers (forgot what this is called).
+var userInput = process.argv[2].toUpperCase();
+
+
+
+//=============================
+//Letter Constructor Function
+function Letter(letterToGuess) {
+    this.letterStore = letterToGuess.toUpperCase(),
+    this.beenGuessed = false
+    this.returnChar = function() {
+        if (this.beenGuessed === true) {
+            console.log(this.letterStore);
+        } else {
+            console.log("_");
+        }
+    }
+    this.checkLetter = function() {
+        if (userInput === this.letterStore) {
+            // Do I need to call the returnChar() function if this is correct?
+            this.beenGuessed = true;
+            this.returnChar()
+        } else {
+            console.log("Incorrect!");
+        }
+    }
+}
+
+
+
+var newLetter = new Letter("G");
+
+console.log(newLetter);
+console.log("==============")
+
+newLetter.checkLetter();
+
+};  module.exports = letterExport;
